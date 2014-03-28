75c6279639842c24b18ba7ee8c3019fbf91a79a7
Sebastian J. Bustamante <sebasj@gmail.com>
2014-03-28 18:08:50 +0100

    sin petes en plantillas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,12 @@
 //= require_tree .
 
 function remove_fields(link) {
-    $(link).previous("input[type=hidden]").value = "1";
-    $(link).up(".files").hide();
+    $(link).previous("input[type=hidden]").value("1");
+    $(link).closest(".fields").hide();
 }
 
 function add_fields(link, association, content) {
     var new_id = new Date().getTime();
     var regexp = RegExp("new_" + association, "g");
-    $(link).up().insert({
-        before: content.replace(regexp, new_id)
-    });
+    $(link).before(content.replace(regexp, new_id));
 }
